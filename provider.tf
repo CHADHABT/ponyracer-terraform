5a6db2a7227d1a504aa50e48d3b974438dad2147
hadilbenmoussa <benmoussahadil22@gmail.com>
2023-07-11 16:32:55 +0200

    adding the state file to the s3 bucket

--- provider.tf
+++ provider.tf
@@ -1,6 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket  = "m2c-terraform"
+    key     = "ponyracer/terraform.tfstate"
+    region  = "eu-west-1"
+    encrypt = true
+  }
+}
 # Setup our aws provider
 provider "aws" {
-  access_key  = "${var.AWS_ACCESS_KEY_ID}"
-  secret_key  = "${var.AWS_SECRET_ACCESS_KEY}"
-  region      = "${var.AWS_REGION}"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region     = var.AWS_REGION
 }
